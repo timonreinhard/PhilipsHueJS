536fa0e1184dcd4d308f8b6c9b213ee638e00b99
James Turner <git@turnerj.com>
2014-03-15 17:14:23 +1030

    Conversion calc changes, API support
    Major conversion calculation changes to be closer to the real light,
    added support for basic communication with a Philips Hue

--- philipshue.js
+++ philipshue.js
@@ -1,10 +1,12 @@
 //More Information: https://github.com/PhilipsHue/PhilipsHueSDK-iOS-OSX/blob/master/ApplicationDesignNotes/RGB%20to%20xy%20Color%20conversion.md
+//https://github.com/Q42/hue-color-converter
+//https://github.com/THEjoezack/ColorMine/blob/master/ColorMine/ColorSpaces/Conversions/XyzConverter.cs
 
 var PhilipsHue = {
-	//eg. Hue = 180, Saturation = 100, Lightness = 50, Type = "hueBulbs"/"livingColors"/"default"
-	HSLToXY: function(Hue, Saturation, Lightness, TypeOrModel)
+	//eg. Hue = 180, Saturation = 100, Luminance = 50, Type = "hueBulbs"/"livingColors"/"default"
+	HSLToXY: function(Hue, Saturation, Luminance, TypeOrModel)
 	{
-		var rgb = this.Utilities.HSLToRGB(Hue, Saturation, Lightness);
+		var rgb = this.Utilities.HSLToRGB(Hue, Saturation, Luminance);
 		return this.RGBToXY(rgb.Red, rgb.Green, rgb.Blue, TypeOrModel);
 	},
 	//eg. Red = 240, Green = 200, Blue = 190, Type = "hueBulbs"/"livingColors"/"default"
@@ -16,14 +18,14 @@ var PhilipsHue = {
 		Blue = Blue / 255;
 
 		//Apply Gamma correction
-		Red = (Red > 0.04045 ? Math.pow((Red + 0.055) / 1.055, 2.4) : Red / 12.92);
-		Green = (Green > 0.04045 ? Math.pow((Green + 0.055) / 1.055, 2.4) : Green / 12.92);
-		Blue = (Blue > 0.04045 ? Math.pow((Blue + 0.055) / 1.055, 2.4) : Blue / 12.92);
+		Red = this.Utilities.PivotRGB(Red);
+		Green = this.Utilities.PivotRGB(Green);
+		Blue = this.Utilities.PivotRGB(Blue);
 
 		//Convert RGB values to XYZ
 		var x = Red * 0.649926 + Green * 0.103455 + Blue * 0.197109;
 		var y = Red * 0.234327 + Green * 0.743075 + Blue * 0.022598;
-		var z = Red * 0.0000000 + Green * 0.053077 + Blue * 1.035763;
+		var z = Red * 0.000000 + Green * 0.053077 + Blue * 1.035763;
 
 		//Calculate XY values from XYZ
 		var cx = x / (x + y + z);
@@ -70,8 +72,10 @@ var PhilipsHue = {
 			cx = closestPoint.X;
 			cy = closestPoint.Y;
 		}
+		
+		var luminance = y;
 
-		return new CGPoint(cx, cy);
+		return new CGPoint(cx, cy, luminance);
 	},
 	XYToRGB: function(Point, TypeOrModel)
 	{
@@ -108,92 +112,92 @@ var PhilipsHue = {
 
 		var x = Point.X;
 		var y = Point.Y;
-		var z = 1.0 - x - y;
+		var z = Point.getZ();
 
-		var Y = 1.0;
+		var Y = Point.Luminance;
 		var X = (Y / y) * x;
-		
+
 		if (isNaN(X))
 		{
 			X = 0;
 		}
-		
+
 		var Z = (Y / y) * z;
-		
+
 		if (isNaN(Z))
 		{
 			Z = 0;
 		}
-
-		var r = X * 3.2406 - Y * 1.5372 - Z * 0.4986;
-		var g = -X * 0.9689 + Y * 1.8758 + Z * 0.0415;
-		var b = X * 0.0557 - Y * 0.2040 + Z * 1.0570;
-
-		if (r > b && r > g && r > 1.0)
-		{
-			//Red is too big
-			g = g / r;
-			b = b / r;
-			r = 1.0;
-		}
-		else if (g > b && g > r && g > 1.0)
-		{
-			//Green is too big
-			r = r / g;
-			b = b / g;
-			g = 1.0;
-		}
-		else if (b > r && b > g && b > 1.0)
-		{
-			//Blue is too big
-			r = r / b;
-			g = g / b;
-			b = 1.0;
-		}
+		
+		var r = X * 1.612 - Y * 0.203 - Z * 0.302;
+		var g = -X * 0.509 + Y * 1.412 + Z * 0.066;
+		var b = X * 0.026 - Y * 0.072 + Z * 0.962;
 
 		//Apply Gamma Correctyion
-		r = r <= 0.0031308 ? 12.92 * r : (1.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
-		g = g <= 0.0031308 ? 12.92 * g : (1.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
-		b = b <= 0.0031308 ? 12.92 * b : (1.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
-
-
-		if (r > b && r > g)
-		{
-			//Red is biggest
-			if (r > 1.0)
-			{
-				g = g / r;
-				b = b / r;
-				r = 1.0;
-			}
-		}
-		else if (g > b && g > r)
-		{
-			//Green is biggest
-			if (g > 1.0)
-			{
-				r = r / g;
-				b = b / g;
-				g = 1.0;
-			}
-		}
-		else if (b > r && b > g)
-		{
-			//Blue is biggest
-			if (b > 1.0)
-			{
-				r = r / b;
-				g = g / b;
-				b = 1.0;
-			}
-		}
-
-		r = Math.min(Math.abs(Math.round(r * 255)), 255);
-		g = Math.min(Math.abs(Math.round(g * 255)), 255);
-		b = Math.min(Math.abs(Math.round(b * 255)), 255);
+		r = this.Utilities.PivotRGB(r, true);
+		g = this.Utilities.PivotRGB(g, true);
+		b = this.Utilities.PivotRGB(b, true);
+		
+		r *= 255;
+		r = Math.round(r > 255 ? 255 : (r < 0 ? 0 : r));
+		
+		g *= 255;
+		g = Math.round(g > 255 ? 255 : (g < 0 ? 0 : g));
+		
+		b *= 255;
+		b = Math.round(b > 255 ? 255 : (b < 0 ? 0 : b));
+		
+		
 		return { Red: r, Green: g, Blue: b };
 	},
+	API: {
+		SendRequestRaw: function(Url, Method, Data, Callback)
+		{
+			var xmlhttp = new XMLHttpRequest();
+			if (typeof Callback == "function")
+			{
+				xmlhttp.onreadystatechange = function()
+				{
+					if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+					{
+						Callback(JSON.parse(xmlhttp.responseText));
+					}
+				}
+			}
+			xmlhttp.open(Method, Url);
+			xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			xmlhttp.send(JSON.stringify(Data));
+		},
+		ChangeState: function(Host, Light, Data, Callback)
+		{
+			this.SendRequestRaw("http://" + Host + "/api/newdeveloper/lights/" + Light + "/state", "PUT", Data, Callback);
+		},
+		SetXY: function(Host, Light, Point, Callback)
+		{
+			var luminance = Math.round(Point.Luminance * 255);
+			this.ChangeState(Host, Light, { "xy" : [ Math.round(Point.X * 10000) / 10000, Math.round(Point.Y * 10000) / 10000 ], "bri": luminance }, Callback);
+		},
+		SetHSL: function(Host, Light, Hue, Saturation, Luminance, Callback)
+		{
+			Hue = Math.round(Hue * (65535 / 360));
+			Saturation = Math.round(Saturation * (255 / 100));
+			Luminance = Math.round(Luminance * (255 / 100));
+			
+			this.ChangeState(Host, Light, { "hue": Hue, "sat": Saturation, "bri": Luminance }, Callback);
+		}
+	},
 	Utilities: {
+		PivotRGB: function(RGBColour, Reverse)
+		{
+			if (typeof Reverse != 'boolean')
+			{
+				return (RGBColour > 0.04045 ? Math.pow((RGBColour + 0.055) / 1.055, 2.4) : RGBColour / 12.92);
+			}
+			else
+			{
+				return (RGBColour <= 0.0031308 ? 12.92 * RGBColour : (1.055) * Math.pow(RGBColour, (1.0 / 2.4)) - 0.055);
+			}
+		},
 		GetColourPointsForModel: function(Model)
 		{
 			var type = Model;
@@ -224,9 +228,9 @@ var PhilipsHue = {
 			if (Type == "hueBulbs")
 			{
 				return {
-					Red: new CGPoint(0.674, 0.322),
+					Red: new CGPoint(0.675, 0.322),
 					Green: new CGPoint(0.408, 0.517),
-					Blue: new CGPoint(0.168, 0.041)
+					Blue: new CGPoint(0.167, 0.04)
 				};
 			}
 			else if (Type == "livingColors")
@@ -267,7 +271,7 @@ var PhilipsHue = {
 				t = 1.0;
 			}
 
-			return new CGPoint(PointA.X + PointB.X * t, PointA.Y + PointB.Y * t);
+			return new CGPoint(PointA.X + ab.X * t, PointA.Y + ab.Y * t);
 		},
 		GetDistanceBetweenTwoPoints: function(Point1, Point2)
 		{
@@ -287,6 +291,41 @@ var PhilipsHue = {
 			var t = this.GetCrossProduct(v1, q) / this.GetCrossProduct(v1, v2);
 
 			return (s >= 0.0 && t >= 0.0 && s + t < 1.0);
+		},
+		XYInReachOfLamp: function(Point, TypeOrModel)
+		{
+			var colourPoints = this.GetColourPointsForModel(TypeOrModel);
+			var inReachOfLamps = this.CheckPointInLampsReach(Point, colourPoints);
+			var x = Point.X, y = Point.Y;
+			
+			if (!inReachOfLamps)
+			{
+				var pAB = this.GetClosestPointToPoints(colourPoints.Red, colourPoints.Green, Point);
+				var pAC = this.GetClosestPointToPoints(colourPoints.Blue, colourPoints.Red, Point);
+				var pBC = this.GetClosestPointToPoints(colourPoints.Green, colourPoints.Blue, Point);
+				var dAB = this.GetDistanceBetweenTwoPoints(Point, pAB);
+				var dAC = this.GetDistanceBetweenTwoPoints(Point, pAC);
+				var dBC = this.GetDistanceBetweenTwoPoints(Point, pBC);
+				var lowest = dAB;
+				var closestPoint = pAB;
+
+				if (dAC < lowest)
+				{
+					lowest = dAC;
+					closestPoint = pAC;
+				}
+
+				if (dBC < lowest)
+				{
+					lowest = dBC;
+					closestPoint = pBC;
+				}
+
+				x = closestPoint.X;
+				y = closestPoint.Y;
+			}
+			
+			return new CGPoint(x, y);
 		},
 		//Method from StackOverflow
 		//http://stackoverflow.com/a/9493060/1676444
@@ -308,25 +347,56 @@ var PhilipsHue = {
 					if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
 					return p;
 				}
-				
+
 				var q = Lightness < 0.5 ? Lightness * (1 + Saturation) : Lightness + Saturation - Lightness * Saturation;
 				var p = 2 * Lightness - q;
 				red = hue2rgb(p, q, Hue + 1/3);
 				red = hue2rgb(p, q, Hue);
 				red = hue2rgb(p, q, Hue - 1/3);
 			}
-			
+
 			red = Math.round(red * 255);
 			green = Math.round(green * 255);
 			blue = Math.round(blue * 255);
-			
+
 			return { Red: red, Green: green, Blue: blue };
+		},
+		//Method from StackOverflow
+		//http://stackoverflow.com/a/9493060/1676444
+		RGBToHSL: function(Red, Green, Blue)
+		{
+			Red /= 255, Green /= 255, Blue /= 255;
+			var max = Math.max(Red, Green, Blue), min = Math.min(Red, Green, Blue);
+			var h, s, l = (max + min) / 2;
+
+			if(max == min)
+			{
+				h = s = 0; // achromatic
+			}
+			else
+			{
+				var d = max - min;
+				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+				switch(max)
+				{
+					case Red: h = (Green - Blue) / d + (Green < Blue ? 6 : 0); break;
+					case Green: h = (Blue - Red) / d + 2; break;
+					case Blue: h = (Red - Green) / d + 4; break;
+				}
+				h /= 6;
+			}
+			return [h, s, l];
 		}
 	}
 };
 
-function CGPoint(X, Y)
+function CGPoint(X, Y, Luminance)
 {
 	this.X = X;
 	this.Y = Y;
+	this.Luminance = Luminance;
+	this.getZ = function getZ()
+	{
+		return 1.0 - this.X - this.Y;
+	}
 }
